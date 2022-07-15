b9f2c0e55cb170af19e053595cb029d3a9f193c4
Sophie <sophie.sa43@gmail.com>
2022-07-15 15:57:50 +0200

    added img src

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>Welcome to Digital Academy</h1>
+     <img src='https://images.unsplash.com/photo-1595206133361-b1fe343e5e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
     </div>
   );
 }
